ad747c06147c7f005eab3240fc0b0c0ddcb8d2b5
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2022-06-03 00:15:51 +0300

    added 53 task: Who likes it?

--- codeWars/day1/index.js
+++ codeWars/day1/index.js
@@ -605,4 +605,22 @@ String.prototype.toJadenCase = function () {
 };
 
 let str = "How can mirrors be real if our eyes aren't real"
-console.log(str.toJadenCase())
+console.log(str.toJadenCase());
+
+//task 53 Who likes it?
+function likes(names) {
+  const str = ' like this'
+ if(names.length === 0) {
+     return ("no one likes this")
+ } else  if(names.length === 1){
+     return (names[0] + " likes this")   
+ } else if(names.length === 2) {
+  return (names[0] + ' and ' + names[1] + str)
+ } else if(names.length === 3) {
+  return (names[0] + ', ' + names[1] + ' and ' + names[2] + str)
+ } else if(names.length > 3) {
+  return (names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others' + str)
+ }
+}
+
+console.log(likes(["Peter", "Alex"]))
